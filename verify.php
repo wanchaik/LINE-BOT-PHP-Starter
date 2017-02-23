f228ed2746ece5b97a712261c1abf0b221701d52
Wanchai Kaekla <wanchaik@hotmail.com>
2017-02-23 23:00:30 +0700

    Fixed

--- verify.php
+++ verify.php
@@ -2,8 +2,8 @@
 $access_token = 'DBUa8Nw4ulhAg1FuFf6aPtULPidvXFg7xH5gehgkIwSUkrzelRRgkZ2h+Y66Qy1byiByGgSLTgLYL3vT6MECxHrkLHZETxL4hoPZ6PwM5k62UnfrWorMpmvmozdxzGDuDZUvWfPvOWolWSday7dtIgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
-
-$headers = array('Authrization: Bearer ' . $access_token);
+                  
+$headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
